98fd0fac75bc55d563cb54c9f5f448c61b6f0538
Srijon Chowdhury <csrijon92@gmail.com>
2025-06-07 23:35:46 +0530

    new change

--- Frontend/src/components/Middle.jsx
+++ Frontend/src/components/Middle.jsx
@@ -7,6 +7,15 @@ const Middle = () => {
         <div className="text-first">
             <h2>Code Online With CompileX</h2>
         </div>
+        <div className="secend-text">
+        <p>This is The Frontend Compiler You Can Write HTML And CSS And JavaScript</p>
+        </div>
+        <div className="button-section">
+          <button className='button' >Populer</button>
+          <button className='button' >Programming</button>
+          <button className='button' >Web</button>
+          <button className='button' >Database</button>
+        </div>
     </section>
   )
 }
