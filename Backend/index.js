9eb7376f0c89ffbee7617e0243fe70b3c266e359
Srijon Chowdhury <csrijon92@gmail.com>
2025-06-29 09:32:29 +0530

    new change

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,31 @@
 import express from "express"
 import fetch from "node-fetch";
+import 'dotenv/config'
+import cors from "cors"
 
 const port = 5000;
 const app = express()
+// app.use(cors())
+const corsoption = {
+  origin: 'http://localhost:5173'
+}
+app.use(cors(corsoption))
+app.use(express.json());
 
 app.get("/",(req,res) => {
   res.send("hello i am a guy")
 })
+app.post("/output/code", (req, res) => {
+  const { language_id, code } = req.body;
+ if (!language_id || !code) {
+    return res.status(400).json({ error: "language_id and code are required" });
+  }
+  try {
+    const url = 
+  } catch (error) {
+    
+  }
+});
 
 app.listen(port,() => {
   console.log(`the server is listen ${port}`)
--- Frontend/src/components/CodeEditorUI.jsx
+++ Frontend/src/components/CodeEditorUI.jsx
@@ -1,56 +1,88 @@
 import React from "react";
+// import fetch from "node-fetch";\
+import { useRef, useState } from 'react';
 import "./CodeEditorUI.css";
 
 const CodeEditorUI = () => {
-  const languageNames = [
-    "Assembly (NASM 2.14.02)",
-    "Bash (5.0.0)",
-    "Basic (FBC 1.07.1)",
-    "C (Clang 7.0.1)",
-    "C++ (Clang 7.0.1)",
-    "C (GCC 7.4.0)",
-    "C++ (GCC 7.4.0)",
-    "C (GCC 8.3.0)",
-    "C++ (GCC 8.3.0)",
-    "C (GCC 9.2.0)",
-    "C++ (GCC 9.2.0)",
-    "Clojure (1.10.1)",
-    "C# (Mono 6.6.0.161)",
-    "COBOL (GnuCOBOL 2.2)",
-    "Common Lisp (SBCL 2.0.0)",
-    "D (DMD 2.089.1)",
-    "Elixir (1.9.4)",
-    "Erlang (OTP 22.2)",
-    "Executable",
-    "F# (.NET Core SDK 3.1.202)",
-    "Fortran (GFortran 9.2.0)",
-    "Go (1.13.5)",
-    "Groovy (3.0.3)",
-    "Haskell (GHC 8.8.1)",
-    "Java (OpenJDK 13.0.1)",
-    "JavaScript (Node.js 12.14.0)",
-    "Kotlin (1.3.70)",
-    "Lua (5.3.5)",
-    "Multi-file program",
-    "Objective-C (Clang 7.0.1)",
-    "OCaml (4.09.0)",
-    "Octave (5.1.0)",
-    "Pascal (FPC 3.0.4)",
-    "Perl (5.28.1)",
-    "PHP (7.4.1)",
-    "Plain Text",
-    "Prolog (GNU Prolog 1.4.5)",
-    "Python (2.7.17)",
-    "Python (3.8.1)",
-    "R (4.0.0)",
-    "Ruby (2.7.0)",
-    "Rust (1.40.0)",
-    "Scala (2.13.2)",
-    "SQL (SQLite 3.27.2)",
-    "Swift (5.2.3)",
-    "TypeScript (3.7.4)",
-    "Visual Basic.Net (vbnc 0.0.0.5943)"
+  const languageOptions = [
+    { id: 45, name: "Assembly (NASM 2.14.02)" },
+    { id: 46, name: "Bash (5.0.0)" },
+    { id: 47, name: "Basic (FBC 1.07.1)" },
+    { id: 75, name: "C (Clang 7.0.1)" },
+    { id: 76, name: "C++ (Clang 7.0.1)" },
+    { id: 48, name: "C (GCC 7.4.0)" },
+    { id: 52, name: "C++ (GCC 7.4.0)" },
+    { id: 49, name: "C (GCC 8.3.0)" },
+    { id: 53, name: "C++ (GCC 8.3.0)" },
+    { id: 50, name: "C (GCC 9.2.0)" },
+    { id: 54, name: "C++ (GCC 9.2.0)" },
+    { id: 86, name: "Clojure (1.10.1)" },
+    { id: 51, name: "C# (Mono 6.6.0.161)" },
+    { id: 77, name: "COBOL (GnuCOBOL 2.2)" },
+    { id: 55, name: "Common Lisp (SBCL 2.0.0)" },
+    { id: 56, name: "D (DMD 2.089.1)" },
+    { id: 57, name: "Elixir (1.9.4)" },
+    { id: 58, name: "Erlang (OTP 22.2)" },
+    { id: 44, name: "Executable" },
+    { id: 87, name: "F# (.NET Core SDK 3.1.202)" },
+    { id: 59, name: "Fortran (GFortran 9.2.0)" },
+    { id: 60, name: "Go (1.13.5)" },
+    { id: 88, name: "Groovy (3.0.3)" },
+    { id: 61, name: "Haskell (GHC 8.8.1)" },
+    { id: 62, name: "Java (OpenJDK 13.0.1)" },
+    { id: 63, name: "JavaScript (Node.js 12.14.0)" },
+    { id: 78, name: "Kotlin (1.3.70)" },
+    { id: 64, name: "Lua (5.3.5)" },
+    { id: 89, name: "Multi-file program" },
+    { id: 79, name: "Objective-C (Clang 7.0.1)" },
+    { id: 65, name: "OCaml (4.09.0)" },
+    { id: 66, name: "Octave (5.1.0)" },
+    { id: 67, name: "Pascal (FPC 3.0.4)" },
+    { id: 85, name: "Perl (5.28.1)" },
+    { id: 68, name: "PHP (7.4.1)" },
+    { id: 43, name: "Plain Text" },
+    { id: 69, name: "Prolog (GNU Prolog 1.4.5)" },
+    { id: 70, name: "Python (2.7.17)" },
+    { id: 71, name: "Python (3.8.1)" },
+    { id: 80, name: "R (4.0.0)" },
+    { id: 72, name: "Ruby (2.7.0)" },
+    { id: 73, name: "Rust (1.40.0)" },
+    { id: 81, name: "Scala (2.13.2)" },
+    { id: 82, name: "SQL (SQLite 3.27.2)" },
+    { id: 83, name: "Swift (5.2.3)" },
+    { id: 74, name: "TypeScript (3.7.4)" },
+    { id: 84, name: "Visual Basic.Net (vbnc 0.0.0.5943)" }
   ];
+  const [selectlanguageid,setselectlanguageid] = useState(languageOptions[0].id)
+  const coderef = useRef(null)
+
+  const handelclick = async () => {
+    let source_code =coderef.current.value;
+
+    const playload = {
+      language_id : selectlanguageid,
+      code:source_code
+    }
+try {
+   const response = await fetch("http://localhost:5000/output/code",{
+      method: "POST",
+      headers :{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(playload)
+    })
+    if(!response.ok){
+    console.error("Server responded with error:", response.status);
+    return;
+  }
+  const data = await response.json();
+  console.log(data)
+} catch (error) {
+   console.error("Fetch failed:", error);
+}
+   
+  }
+
 
   return (
     <div className="editor-wrapper">
@@ -59,18 +91,18 @@ const CodeEditorUI = () => {
         <h3 className="editor-title">Python Hello World</h3>
         <div className="editor-controls">
           <button className="btn-green">  AI</button>
-          <select className="lang-select">
-            {languageNames.map((lan, index) => (
-              <option key={index}>{lan}</option>
+          <select className="lang-select" value={selectlanguageid} onChange={(e) => setselectlanguageid(Number(e.target.value))} >
+            {languageOptions.map((lan, index) => (
+              <option value={lan.id} key={index}>{lan.name}</option>
             ))}
           </select>
-          <button className="btn-run">RUN ▶</button>
+          <button onClick={handelclick} className="btn-run">RUN ▶</button>
         </div>
       </div>
 
       <div className="editor-main">
         <div className="code-area">
-          <textarea defaultValue={`print("Hello, World!")`} />
+          <textarea ref={coderef} defaultValue={`print("Hello, World!")`} />
         </div>
 
         <div className="side-panel">
