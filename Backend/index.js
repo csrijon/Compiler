f4627a7bfcf29792b71d5e7e0e25d45fb8640013
Srijon Chowdhury <csrijon92@gmail.com>
2025-06-29 17:44:03 +0530

    new change

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { json } from "express"
 import fetch from "node-fetch";
 import 'dotenv/config'
 import cors from "cors"
@@ -12,21 +12,38 @@ const corsoption = {
 app.use(cors(corsoption))
 app.use(express.json());
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
   res.send("hello i am a guy")
 })
-app.post("/output/code", (req, res) => {
+app.post("/output/code", async (req, res) => {
   const { language_id, code } = req.body;
- if (!language_id || !code) {
+  if (!language_id || !code) {
     return res.status(400).json({ error: "language_id and code are required" });
   }
-  try {
-    const url = 
-  } catch (error) {
-    
-  }
+    const url = process.env.API_URL
+    const option = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'x-rapidapi-key':process.env.API_KEY,
+        'x-rapidapi-host' :process.env.API_HOST
+      },
+       body: JSON.stringify({
+         source_code: code,
+      language_id: language_id
+       })
+    }
+   try {
+    const responsedata = await fetch(url,option);
+    const resdata = await responsedata.json()
+    console.log(resdata)
+    res.status(200).json(resdata);
+   } catch (error) {
+   console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+   }
 });
 
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`the server is listen ${port}`)
 })
