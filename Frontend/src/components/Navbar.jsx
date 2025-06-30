97dba40cefce773a91e3fd899bd631a6521109de
Srijon Chowdhury <csrijon92@gmail.com>
2025-07-01 01:47:17 +0530

    new change

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -13,7 +14,7 @@ const Navbar = () => {
 
         <ul className={`nav-links ${isOpen ? "active" : ""}`}>
           <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
+          <li><Link to="/Compile">Compiler</Link></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a href="">Login</a></li>
